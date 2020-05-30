28e35fb537b480212cfb5fe9264511162be941c7
PrimaryCY <907031027@qq.com>
2020-05-30 17:40:04 +0800

    修改.gitignore文件,将前端spa页面下线

--- sheep-vue/src/plugins/element/element.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-//导入elementUI
-import './theme/index.css'
-import ElementUI from 'element-ui'
-// import './plugins/element.js'
-
-Vue.use(ElementUI)
--- sheep-vue/src/store/state.js
+++
@@ -1,5 +0,0 @@
-
-export default {
-  user:{},                  //用户个人信息
-  option:{},                //远程公共配置
-}
